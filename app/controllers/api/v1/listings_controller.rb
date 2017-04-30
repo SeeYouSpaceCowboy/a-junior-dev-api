0336a1964bbbbcc8a2bda94b859c77ce6a74aaa2
Mohammed Chisti <Chisti@Mohammeds-MacBook-Air.local>
2017-04-30 13:22:40 -0400

    Allowed people to be able to post a job posting.

--- app/controllers/api/v1/listings_controller.rb
+++ app/controllers/api/v1/listings_controller.rb
@@ -8,15 +8,15 @@ class Api::V1::ListingsController < ApplicationController
   def create
     @listing = Listing.create(listing_params)
 
-    if @listings
+    if @listing
       render json: @listing
     else
-      render json: { error: "Failed to create listings"}, status: 400
+      render json: { error: "Failed to create listing"}, status: 400
     end
   end
 
   private
     def listing_params
-      params.require(:listing).permit(:position, :company, :company_url, :apply_url, :type, :experience_level, :compensation, :street, :city, :state, :zipcode, :programming_languages, :description)
+      params.require(:listing).permit(:position, :company, :company_url, :apply_url, :shift, :experience_level, :compensation, :street, :city, :state, :zipcode, :programming_languages, :description)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
       resources :users, only: [:index, :destroy]
       get 'user', to: 'users#show', as: 'user_show'
 
-      resources :listings, only: [:index, :update, :destroy]
+      resources :listings, only: [:index, :create, :update, :destroy]
     end
   end
 end
